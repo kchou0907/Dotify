0bc17a2e582617cad50913576491ddba35117a44
Kevin Chou <kchou0907@gmail.com>
2021-04-13 00:58:47 -0700

    implemented barebones widgets and features

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dotify Homework 1 - Kevin Chou
+This is a rough layout of a music streaming app. None of the functions actually work, but it gives you a good idea of what it's going to look like
+
+## Extra Credit
+- User is not allowed to apply new username if the text field is empty
+- Using constraint layout, utilize a barrier
+
+## Pic of it Working
+![Home Screen](screenshots/pic.png)
--- app/src/main/java/kchou97/dotify/MainActivity.kt
+++ app/src/main/java/kchou97/dotify/MainActivity.kt
@@ -2,10 +2,98 @@ package kchou97.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val randomNumber = Random.nextInt(1000, 10000)
+    private var playNum = 0
+    private lateinit var playCount: TextView
+    private lateinit var prevBtn: ImageButton
+    private lateinit var nextBtn: ImageButton
+    private lateinit var playBtn: ImageButton
+    private lateinit var editBtn: Button
+    private lateinit var submitBtn: Button
+    private lateinit var userfield: TextView
+    private lateinit var textInput: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playCount = findViewById(R.id.playCount)
+        prevBtn = findViewById(R.id.prev)
+        nextBtn = findViewById(R.id.next)
+        playBtn = findViewById(R.id.play)
+        editBtn = findViewById(R.id.changeUser)
+        submitBtn = findViewById(R.id.submitUser)
+        userfield = findViewById(R.id.username)
+        textInput = findViewById(R.id.editUsername)
+        var submitIsVisible: Boolean = true;
+
+        playCount.text = randomNumber.toString() + " plays"
+        playNum = randomNumber
+
+        submitBtn.visibility = View.GONE
+        textInput.visibility = View.GONE
+
+        prevBtn.setOnClickListener{
+            displayMessage("Skipping to previous track")
+        }
+
+        nextBtn.setOnClickListener{
+            displayMessage("Skipping to next track")
+        }
+
+        playBtn.setOnClickListener{
+            incrementCounter(playCount)
+        }
+
+        editBtn.setOnClickListener{
+            toggleEdit(submitBtn, textInput, editBtn, userfield, submitIsVisible)
+            submitIsVisible = !submitIsVisible
+        }
+
+        submitBtn.setOnClickListener{
+            if (editUserName(textInput, userfield)) {
+                toggleEdit(submitBtn, textInput, editBtn, userfield, !submitIsVisible)
+                submitIsVisible = !submitIsVisible
+            } else {
+                displayMessage("Please submit a valid username")
+            }
+        }
+    }
+    fun displayMessage(message: String) {
+        Toast.makeText(this, message,Toast.LENGTH_SHORT).show()
+    }
+    fun incrementCounter(playCounter: TextView) {
+        playNum++
+        playCounter.text = playNum.toString() + " plays"
+    }
+    fun editUserName(textInput: EditText, userfield: TextView): Boolean{
+        var newUsername = textInput.editableText
+        if (newUsername.length == 0) {
+            return false
+        } else {
+            userfield.text = newUsername
+            return true
+        }
+
+    }
+    fun toggleEdit(submitBtn: Button, textInput: EditText, editBtn: Button, userfield: TextView, submitIsVisible: Boolean) {
+        if (submitIsVisible) {
+            editBtn.visibility = View.GONE
+            userfield.visibility = View.GONE
+
+            submitBtn.visibility = View.VISIBLE
+            textInput.visibility = View.VISIBLE
+        } else {
+            editBtn.visibility = View.VISIBLE
+            userfield.visibility = View.VISIBLE
+
+            submitBtn.visibility = View.GONE
+            textInput.visibility = View.GONE
+        }
     }
 }

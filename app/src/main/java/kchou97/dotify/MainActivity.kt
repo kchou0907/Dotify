b9296b8490812c7df476c29a16195447e3bb515f
Kevin Chou <kchou0907@gmail.com>
2021-04-12 22:40:58 -0700

    created front end

---
+++ app/src/main/java/kchou97/dotify/MainActivity.kt
@@ -0,0 +1,11 @@
+package kchou97.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
